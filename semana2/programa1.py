8e0882b3fd34af347498096fba39a373e0abe943
AxelCarrillo1 <122809603+AxelCarrillo@users.noreply.github.com>
2023-01-23 17:00:06 +0000

    CREATE carpeta semana2 y progrma1.py

---
+++ semana2/programa1.py
@@ -0,0 +1,10 @@
+""" 
+   Programa1
+   Nombre. Axel CR
+   Fecha: 23/01/2023
+   Descripcion: Comentarios Multilinea, Comentarios de una Linea, Concatenacion de cadenas y 
+   cadeana de caracteres
+"""
+vari1="Hola " #Variable que almacena una cadena de caracteres
+vari2="Python" #Variable que almacena una cadena de caracteres 
+print(vari1 + vari2) #Concatena 2 caracteres
