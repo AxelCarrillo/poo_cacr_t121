5eb18639e336ff8ec675cf66d74f8d260d504945
AxelCarrillo1 <122809603+AxelCarrillo@users.noreply.github.com>
2023-01-24 19:57:26 +0000

    CREATE programa2.py

---
+++ semana2/programa2.py
@@ -0,0 +1,10 @@
+"""
+   Programa2
+   Nombre. Axel CR
+   Fecha: 24/01/2023
+   Descripcion: Comentarios Multilinea, Comentarios de una Linea, Concatenacion de cadenas y 
+   cadeana de caracteres
+"""
+vari1="Hola " 
+vari2= 619  
+print("{} {}".format(vari1,vari2)) #El format nos permite formatear valores y no importa el tipo de dato que estemos utilizando
