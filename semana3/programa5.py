5c4bcdcdd2c1a8d5517487ad7bd74aca8bf4010f
AxelCarrillo1 <122809603+AxelCarrillo@users.noreply.github.com>
2023-01-30 17:00:29 +0000

    CREATED suma,res,etc

---
+++ semana3/programa5.py
@@ -0,0 +1,22 @@
+
+
+num1= input("Numero 1:")
+num2= 10
+suma= int(num1) +  num2
+resta= int(num1) - num2
+mult= int(num1) * num2
+divi= int(num1) / num2
+pote= int(num1) ** num2
+
+print ("La suma de {} + {} es = {} ".format(num1,num2,suma))
+print ("La resta de {} - {} es = {} ".format(num1,num2,resta))
+print ("La multiplicacion de {} * {} es = {} ".format(num1,num2,mult))
+print ("La division de {} + {} es = {} ".format(num1,num2,suma))(divi)
+print (pote)
+
+
+
+
+
+
+
