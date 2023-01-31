12af2e23f0023afc0bdf309d3eaeab52db24d3af
AxelCarrillo1 <122809603+AxelCarrillo@users.noreply.github.com>
2023-01-31 19:27:33 +0000

    CREATED area y perimetro del triangulo

---
+++ semana3/programa6.py
@@ -0,0 +1,11 @@
+
+print ("Area y Perimetro de un Triangulo")
+ar= float(input("Digita el tamaño del lado: "))
+bas= float(input("Digita el tamaño de la base: "))
+h= float(input("Digita el tamaño de la altura: "))
+
+area= (bas*h)/2
+peri = ar*3
+
+print("El área del triangulo es: ", area)
+print("El perímetro del triangulo es: ", peri)
