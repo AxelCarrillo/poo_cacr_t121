3455e4becd4ad5ab69d53e370f8540b372fd4879
AxelCarrillo1 <122809603+AxelCarrillo@users.noreply.github.com>
2023-02-02 20:39:57 +0000

    CREATED ingresa un numero y que imprima si es mayor o igual

---
+++ semana3/programa9.py
@@ -0,0 +1,16 @@
+"""
+   programa7.py
+   Nombre. Axel CR
+   Fecha: 02/02/2023
+   Descripcion: Comparacion de 2 numeros y si son iguales que lo imprima
+"""
+n1= int(input("Digita un numero: "))
+n2= int(input("Digita un segundo numero: ")) 
+
+
+if n1 > n2:
+    print ("Su numero mayor es: ",n1)
+elif n2>n1:
+    print ("Su numero mayor es: ",n2)
+else:
+    print (None)
